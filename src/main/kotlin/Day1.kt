b9a9db440c48b0649b977c0f54e7a5c895b1f5de
Marc Poppleton <marc.poppleton@gmail.com>
2020-12-01 14:30:19 +0100

    cleaning up a bit of useless code for the sake of clarity

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -11,8 +11,8 @@ class Day1 {
         }
         try {
             val entries = getEntries(args[1])
-            doMagicWith2(2020, entries)
-            doMagicWith3(2020, entries)
+            println("Magic with 2 is : ${doMagicWith2(2020, entries)}")
+            println("Magic with 3 is : ${doMagicWith3(2020, entries)}")
         }catch(e:java.io.FileNotFoundException){
             println("${args[1]} is not a valid file.")
         }
@@ -26,23 +26,17 @@ class Day1 {
     fun doMagicWith2(sum: Int, entries: IntArray): Int {
         //sort out the damn entries, we don't need any above the sum target value
         val culled = entries.filter { i -> i < sum }.sorted()
-        //convenient boolean used later
-        var match = false
-        var answer = -1
-        culled.asReversed().forEach lit1@{ big ->
+        culled.asReversed().forEach { big ->
             //no need to carry on if matching pair is found
-            if (match) return@lit1
             culled.forEach lit2@{ small ->
                 //if matching pair is found or tested pair sum is above target value, we can stop iterating
-                if ((match)||(big + small > sum)) return@lit2
+                if (big + small > sum) return@lit2
                 if (big + small == sum) {
-                    println("$big + $small = $sum. Product is ${big * small}")
-                    answer = big * small
-                    match = true
+                    return big * small
                 }
             }
         }
-        return answer
+        return -1
     }
 
     /*
@@ -50,24 +44,17 @@ class Day1 {
      */
     fun doMagicWith3(sum: Int, entries: IntArray): Int {
         val culled = entries.filter { i -> i < sum }.sorted()
-        var match = false
-        var answer = -1
-        culled.asReversed().forEach lit1@{ big ->
-            if (match) return@lit1
-            culled.asReversed().forEach lit2@{ middle ->
-                if (match) return@lit2
+        culled.asReversed().forEach { big ->
+            culled.asReversed().forEach { middle ->
                 culled.forEach lit3@{ small ->
-                    if ((match)||(big + middle + small > sum)) return@lit3
+                    if (big + middle + small > sum) return@lit3
                     if (big + middle + small == sum) {
-                        println("$big + $middle + $small = $sum. Product is ${big * middle * small}")
-                        answer = big * middle * small
-                        match = true
+                        return big * middle * small
                     }
                 }
             }
-
         }
-        return answer
+        return -1
     }
 
     fun getEntries(filename: String): IntArray = File(filename).readLines().map { line -> line.toInt() }.toIntArray()
